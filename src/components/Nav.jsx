3436e3a17f66063a5b5765c2e7ad2f0e153c3827
nandovbr <nandovbr15@hotmail.com>
2023-02-23 21:29:20 -0300

    estilização do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData, BsBriefcase, BsChatSquare } from "react-icons/bs";
+// link
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+      <div className='container mx-auto'>
+        <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1
+          rounded-full max-w-[460px] mx-auto px-5 flex justify-between 
+          items-center text-2x1 text-white/50'>
+
+          <Link className='cursor-pointer w-[60px] h-[60px] flex items-center
+            justify-center'>
+            <BiHomeAlt />
+          </Link>
+
+          <Link className='cursor-pointer w-[60px] h-[60px] flex items-center
+            justify-center'>
+            <BiUser />
+          </Link>
+
+          <Link className='cursor-pointer w-[60px] h-[60px] flex items-center
+            justify-center'>
+            <BsClipboardData />
+          </Link>
+
+          <Link className='cursor-pointer w-[60px] h-[60px] flex items-center
+            justify-center'>
+            <BsBriefcase />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
