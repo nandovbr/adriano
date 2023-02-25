714b73211a84b0cc3f8809e341d185f00e8b7371
nandovbr <nandovbr15@hotmail.com>
2023-02-24 21:23:16 -0300

    estilização da página de Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,123 @@
 import React from "react";
+// motion e variant
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+// imagens
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 export default function Work() {
   return (
-    <div className='section' id='work'>
-      Work
-    </div>
+    <section className='section' id='work'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row gap-x-10'>
+          <motion.div
+            variants={ fadeIn('right', 0.3) }
+            initial='hidden'
+            whileInView={ 'show' }
+            viewport={ { once: false, amount: 0.3 } }
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+            {/* texto */}
+            <div>
+              <h2 className='h2 leading-tight text-accent'>
+                Meu último <br />
+                Trabalho
+              </h2>
+              <p className='max-w-sm mb-16'>
+                Jesus é o Rei da Glória Jesus é o Rei da Glória Jesus é o Rei da Glória 
+                Jesus é o Rei da Glória Jesus é o Rei da Glória Jesus é o Rei da Glória 
+                Jesus é o Rei da Glória Jesus é o Rei da Glória Jesus é o Rei da Glória 
+              </p>
+
+              <button className='btn btn-sm'>Todas as Músicas</button>
+
+            </div>
+            {/* imagem */}
+            <div className='group relative overflow-hidden border-2
+            border-white/50 rounded-x1'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute
+              z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={ Img1 }
+                alt='imagem do projeto'
+              />
+              {/* pre titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24
+                transition-all duration-500 z-50'
+              >
+                <span className='text-gradient '>Rei da Glória</span>
+              </div>
+              {/* titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14
+                transition-all duration-700 z-50'
+              >
+                <span className='text-3x1 text-white'>Título do Projeto</span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={ fadeIn('left', 0.2) }
+            initial='hidden'
+            whileInView={ 'show' }
+            viewport={ { once: false, amount: 0.3 } }
+            className='flex-1 flex flex-col gap-y-10'>
+            {/* imagem */}
+            <div className='group relative overflow-hidden border-2
+            border-white/50 rounded-x1'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute
+              z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={ Img2 }
+                alt='imagem do projeto'
+              />
+              {/* pre titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24
+                transition-all duration-500 z-50'
+              >
+                <span className='text-gradient '>Rei da Glória</span>
+              </div>
+              {/* titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14
+                transition-all duration-700 z-50'
+              >
+                <span className='text-3x1 text-white'>Título do Projeto</span>
+              </div>
+            </div>
+            {/* imagem */}
+            <div className='group relative overflow-hidden border-2
+            border-white/50 rounded-x1'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute
+              z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img
+                className='group-hover:scale-125 transition-all duration-500'
+                src={ Img3 }
+                alt='imagem do projeto'
+              />
+              {/* pre titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24
+                transition-all duration-500 z-50'
+              >
+                <span className='text-gradient '>Rei da Glória</span>
+              </div>
+              {/* titulo */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14
+                transition-all duration-700 z-50'
+              >
+                <span className='text-3x1 text-white'>Título do Projeto</span>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
